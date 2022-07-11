603fde47802eb2e882bb511ac1975ef5597c5eb5
chaos0103 <lyt1228@naver.com>
2022-07-12 02:12:52 +0900

    feat: 계좌거래내역 서비스 구현
    계좌이체 기능 구현

--- src/main/java/toyproject/bank/service/AccountTransactionService.java
+++ src/main/java/toyproject/bank/service/AccountTransactionService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toyproject.bank.domain.Account;
 import toyproject.bank.domain.AccountTransaction;
+import toyproject.bank.domain.TransactionType;
 import toyproject.bank.dto.AccountTransactionDto;
 import toyproject.bank.exception.NoSuchException;
 import toyproject.bank.repository.AccountRepository;
@@ -19,12 +20,23 @@ public class AccountTransactionService {
     private final AccountRepository accountRepository;
 
     /**
-     * 거래
+     * 입출금거래
      */
     public Long transaction(String accountId, AccountTransactionDto trDto) {
         Account account = getAccount(accountId);
         AccountTransaction savedAt = atRepository.save(new AccountTransaction(account, trDto.getAmount(), trDto.getType(), trDto.getContent()));
         return savedAt.getId();
+    }
+
+    /**
+     * 계좌이체
+     */
+    public void accountTransaction(String fromAccountId, String toAccountId, AccountTransactionDto trDto) {
+        Account fromAccount = getAccount(fromAccountId);
+        Account toAccount = getAccount(toAccountId);
+
+        atRepository.save(new AccountTransaction(fromAccount, trDto.getAmount(), TransactionType.OUT, trDto.getContent()));
+        atRepository.save(new AccountTransaction(toAccount, trDto.getAmount(), TransactionType.IN, trDto.getContent()));
     }
 
     private Account getAccount(String accountId) {

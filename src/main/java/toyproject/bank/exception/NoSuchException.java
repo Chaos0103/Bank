ae31a246544c4b0c8ff9c31602f5d6743634d0be
chaos0103 <lyt1228@naver.com>
2022-07-11 23:01:06 +0900

    feat: 고객 서비스 설계
    ClientService, ClientRepository 설계
    고객 등록, 수정, 삭제 구현

---
+++ src/main/java/toyproject/bank/exception/NoSuchException.java
@@ -0,0 +1,20 @@
+package toyproject.bank.exception;
+
+public class NoSuchException extends IllegalStateException {
+
+    public NoSuchException() {
+        super();
+    }
+
+    public NoSuchException(String s) {
+        super(s);
+    }
+
+    public NoSuchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoSuchException(Throwable cause) {
+        super(cause);
+    }
+}
